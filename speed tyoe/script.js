6533e2e65d2af3e11077e7fdfd25f0a98b1c0aad
saiking1998 <luckysai998@gmail.com>
2022-02-20 22:10:42 +0530

    gg

--- speed tyoe/script.js
+++ speed tyoe/script.js
@@ -3,7 +3,7 @@ const apiurl = 'https://api.quotable.io/random'
 const quotedisplay = document.getElementById('quotedisp')
 const quoteinput = document.getElementById('quotein')
 const timer = document.getElementById('timer')
-
+const area={}
 quoteinput.addEventListener('input', () => {
     const arrayquote = quotedisplay.querySelectorAll('span')
     const arrayvalue = quoteinput.value.split('')
@@ -19,17 +19,25 @@ quoteinput.addEventListener('input', () => {
         else if (char === charspand.innerText) {
             charspand.classList.add('correct')
             charspand.classList.remove('incorrect')
+          
         }
         else {
             charspand.classList.add('incorrect')
             charspand.classList.remove('correct')
             correct = false
+        
+           
         }
     })
-    if (correct) {
-        getnewapi()
+    // if (correct) {
+    //     getnewapi()
+    // }
+    if(arrayquote.length<=arrayvalue.length){
+        clearInterval(area.timer)
+        setTimeout(getnewapi,3000)
     }
 
+  
 })
 function newapi() {
     return fetch(apiurl)
@@ -55,12 +63,16 @@ let starttime
 function timerstart() {
     timer.innerText = 0
     starttime = new Date
-    setInterval(() => {
+    area.timer=setInterval(() => {
         timer.innerText = gettime()
     }, 1000)
 }
+
 function gettime() {
-    return Math.floor((new Date - starttime) / 1000)
+       
+            return Math.floor((new Date - starttime) / 1000)  
+       
+    
 }
 getnewapi()
 
