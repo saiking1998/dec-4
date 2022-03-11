cc6a17bf1b0025ceb6a5063198c1ebae58370a2d
saiking1998 <luckysai998@gmail.com>
2022-03-11 14:51:23 +0530

    gg

--- react3/src/App.js
+++ react3/src/App.js
@@ -1,17 +1,22 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Container,Button,ProgressBar} from 'react-bootstrap';
+import React,{useState} from 'react';
 import TableData from './data/TableData';
 
 
 function App() {
+  const [show,setShow]= useState(10);
+ 
   return (
-    <div className="App">
-      <TableData />
-     
-    
-    </div>
+    <Container className="App">
+      {/* {show &&  <TableData />}
+      {show}
+      <Button variant="warning" onClick={()=>setShow(show+1)}>{show}</Button> */}
+     <ProgressBar now={show} label={`${show}%`} />
+     <Button variant="success" onClick={()=>setShow(show+5)}>click to increment</Button>
+    </Container>
   );
 }
 
