95cf001a3c081b348fbb78998df438109582bf62
saiking1998 <luckysai998@gmail.com>
2022-03-13 18:21:42 +0530

    gg

---
+++ recat-apps/react-5/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Post from './components/Post'
+import {Container} from 'react-bootstrap';
+
+function App() {
+  return (
+  <Container>
+     <Post />
+  </Container>
+  );
+}
+
+export default App;
